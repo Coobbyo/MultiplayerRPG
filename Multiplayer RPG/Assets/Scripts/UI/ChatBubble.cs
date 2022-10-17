86b25854cd06ad1d0385ee1ae5bce368eb5789f9
Coobbyo <61762261+Coobbyo@users.noreply.github.com>
2022-10-17 12:48:49 -0700

    Chat Bubbles
    Continued implementation of Code Monkey chat bubbles.
    
    Added animated text reading
    working on text window

--- Multiplayer RPG/Assets/Scripts/UI/ChatBubble.cs
+++ Multiplayer RPG/Assets/Scripts/UI/ChatBubble.cs
@@ -56,6 +56,8 @@ public class ChatBubble : MonoBehaviour
         backgroundSpriteRenderer.transform.localPosition = new Vector3(backgroundSpriteRenderer.size.x / 2f, 0f) + offset;
 
         iconSpriteRenederer.sprite = GetIconSprite(iconType);
+
+        TextWriter.AddWriter_Static(textMeshPro, text, 0.05f, true, true, () => { });
     }
 
     private Sprite GetIconSprite(IconType iconType)
